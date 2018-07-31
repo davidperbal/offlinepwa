efdcbebfc18cea41d6543a970de275a9e69a7064
davidperbal <41942649+davidperbal@users.noreply.github.com>
2018-07-31 10:44:19 +0200

    New fetch

--- sw.js
+++ sw.js
@@ -47,10 +47,37 @@ self.addEventListener('active', function activator(event) {
 /**
  * Intercepet request cache.
  */
-self.addEventListener('fetch', function(event) {
+/*self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function (cachedResponse) {
       return cachedResponse || fetch(event.request);
     })
   );
+});*/
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
 });
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
